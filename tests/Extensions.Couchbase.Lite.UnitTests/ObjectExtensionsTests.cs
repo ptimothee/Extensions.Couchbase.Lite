c78d7f23dec791bf305f26c2e3fa25cef1254f7d
Patrick Timothee <ptimothee@junipercommerce.com>
2024-12-10 12:29:25 -0500

    Added ObjectExtensions to automatically convert to to a MutableDocument

---
+++ tests/Extensions.Couchbase.Lite.UnitTests/ObjectExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Codemancer.Extensions.Couchbase.Lite.UnitTests.Fixtures;
+using Codemancer.Extensions.Couchbase.Lite.UnitTests.Fixtures.Models;
+
+namespace Codemancer.Extensions.Couchbase.Lite.Tests;
+
+public class ObjectExtensionsTests
+{
+    [Fact]
+    [Trait("category", "unit-test")]
+    public void ToMutableDocument_WhenInstanceIsNull_ShouldReturnMutableDocumentWithGeneratedId()
+    {
+        // Arrange
+        Person? person = null;
+
+        // Act
+        var document = person.ToMutableDocument();
+
+        // Assert
+        document.Id.Should().NotBeNullOrEmpty();
+        document.Keys.Should().BeEmpty();
+    }
+
+    [Theory]
+    [Trait("category", "unit-test")]
+    [MemberData(nameof(ObjectExtensionsTestFixture.GetSupportedMutableObjects), MemberType = typeof(ObjectExtensionsTestFixture))]
+    public void ToMutableDocument_ShouldReturnMutableDocumentWithExpected(object? obj, Dictionary<string, object?> expectedDictionary)
+    {
+        // Act
+        var document = obj.ToMutableDocument();
+
+        // Assert
+        document.Id.Should().NotBeNullOrEmpty();
+        document.ToDictionary().Should().BeEquivalentTo(expectedDictionary);
+    }
+}
