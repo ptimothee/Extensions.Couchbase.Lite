b66ad3bda3dda0017f481ea7728f7e8d65ac2344
Patrick Timothee <ptimothee@junipercommerce.com>
2024-12-10 12:31:43 -0500

    removed redundant test

--- tests/Extensions.Couchbase.Lite.UnitTests/ObjectExtensionsTests.cs
+++ tests/Extensions.Couchbase.Lite.UnitTests/ObjectExtensionsTests.cs
@@ -1,26 +1,10 @@
 ﻿using FluentAssertions;
 using Codemancer.Extensions.Couchbase.Lite.UnitTests.Fixtures;
-using Codemancer.Extensions.Couchbase.Lite.UnitTests.Fixtures.Models;
 
 namespace Codemancer.Extensions.Couchbase.Lite.Tests;
 
 public class ObjectExtensionsTests
 {
-    [Fact]
-    [Trait("category", "unit-test")]
-    public void ToMutableDocument_WhenInstanceIsNull_ShouldReturnMutableDocumentWithGeneratedId()
-    {
-        // Arrange
-        Person? person = null;
-
-        // Act
-        var document = person.ToMutableDocument();
-
-        // Assert
-        document.Id.Should().NotBeNullOrEmpty();
-        document.Keys.Should().BeEmpty();
-    }
-
     [Theory]
     [Trait("category", "unit-test")]
     [MemberData(nameof(ObjectExtensionsTestFixture.GetSupportedMutableObjects), MemberType = typeof(ObjectExtensionsTestFixture))]
