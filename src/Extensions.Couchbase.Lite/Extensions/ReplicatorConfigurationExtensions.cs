bf67ad7e95575e80810c207c3c558976036419de
Patrick Timothee <ptimothee@junipercommerce.com>
2025-01-10 20:51:23 -0500

    Refactored Extensions class into ReplicatorConfigurationExtensions

---
+++ src/Extensions.Couchbase.Lite/Extensions/ReplicatorConfigurationExtensions.cs
@@ -0,0 +1,49 @@
+﻿using Couchbase.Lite.Sync;
+
+namespace Codemancer.Extensions.Couchbase.Lite.Extensions
+{
+    public static class ReplicatorConfigurationExtensions
+    {
+        public static Uri GetHttpEndpoint(this ReplicatorConfiguration configuration)
+        {
+            return configuration.Target.AsHttpUri();
+        }
+
+        public static string GetEndpointName(this ReplicatorConfiguration configuration)
+        {
+            if (configuration.Target is not URLEndpoint urlEndpoint)
+            {
+                throw new InvalidOperationException("Replicator target is not a URLEndpoint");
+            }
+            return urlEndpoint.Url.GetEndpointName();
+        }
+
+        private static Uri AsHttpUri(this IEndpoint endpoint)
+        {
+            if(endpoint is not URLEndpoint urlEndpoint)
+            {
+                throw new InvalidOperationException("Endpoint is not a URLEndpoint.");
+            }
+            
+            // Ensure trailing slash
+            var path = urlEndpoint.Url.LocalPath;
+            if (!path.EndsWith('/'))
+            {
+                path += '/';
+            }
+
+            var uriBuilder = new UriBuilder(urlEndpoint.Url)
+            {
+                Scheme = "https", // Switch scheme to https
+                Path = path
+            };
+
+            return uriBuilder.Uri;
+        }
+
+        private static string GetEndpointName(this Uri uri)
+        {
+            return uri.Segments.Last().Trim('/');
+        }
+    }
+}
