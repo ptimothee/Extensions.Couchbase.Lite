6275e8ef3d66bba661e6c017ab77861bd65b59a9
Patrick Timothee <patrick.timothee@gmail.com>
2025-01-20 23:04:23 -0500

    Implemented jwt authentication via authorization header (#7)

---
+++ src/Extensions.Couchbase.Lite/Sync/AuthenticationMethod.cs
@@ -0,0 +1,8 @@
+﻿namespace Codemancer.Extensions.Couchbase.Lite.Sync;
+
+public enum AuthenticationMethod
+{
+    SessionProvider = 0,
+    AuthorizationHeader = 1,
+}
+
---
+++ src/Extensions.Couchbase.Lite/Sync/UsernameBuildOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Codemancer.Extensions.Couchbase.Lite.Sync;
+
+public class UsernameBuildOptions(string claimName, string prefix)
+{
+    public string Prefix { get; set; } = prefix;
+
+    public string ClaimType { get; set; } = claimName;
+}
+
